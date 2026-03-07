1a982291921776fa3ceda406c94df02388de2deb
UnEmotioneD <blackeagle10@icloud.com>
2026-03-07 12:36:21 +0900

    rename: install.sh -> master.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-. ./bin/install-git.sh
-. ./bin/install-yay.sh
-. ./bin/clone-dotfiles.sh
---
+++ master.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source bin/install-yay.sh
+source bin/clone-dotfiles.sh
