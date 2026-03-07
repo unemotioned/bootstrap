cb1a682e835b43edc067d1905683647948f79e18
UnEmotioneD <blackeagle10@icloud.com>
2026-03-08 00:08:35 +0900

    update: master.sh

--- master.sh
+++ master.sh
@@ -2,5 +2,15 @@
 
 set -euo pipefail
 
-source bin/install-yay.sh
-source bin/clone-dotfiles.sh
+. ./bin/install-yay.sh
+
+. ./bin/install-reflector.sh
+# . ./bin/sort-mirror-list.sh # comment out when testing
+
+. ./bin/install-zsh.sh
+. ./bin/change-shell.sh
+
+. ./bin/install-stow.sh
+. ./bin/clone-dotfiles.sh
+
+. ./bin/remove-orphaned.sh
