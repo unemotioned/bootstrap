264bcb8dafc455e707f8a3d9ef8910b7586f5b2f
UnEmotioneD <blackeagle10@icloud.com>
2026-03-09 18:10:37 +0900

    fix: bt-setup - intall bluez-utiles & enable service

--- bin/bt-setup.sh
+++ bin/bt-setup.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
-sudo pacman -S --noconfirm --needed bluez
+sudo pacman -S --noconfirm --needed\
+  bluez\
+  bluez-utils
 
 yay -S --noconfirm --needed overskride
+
+sudo systemctl start bluetooth.service
