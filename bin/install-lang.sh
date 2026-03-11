0cbaed7f1d04e3715431dc9a5e2fe410543867da
UnEmotioneD <blackeagle10@icloud.com>
2026-03-11 19:32:07 +0900

    feat: remove rust before installing rustup

--- bin/install-lang.sh
+++ bin/install-lang.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo pacman -S --noconfirm --needed\
-  jdk17-openjdk\
-  rustup
+if pacman -Qi rust &>/dev/null; then
+  sudo pacman -Rns --noconfirm rust
+fi
+
+sudo pacman -S --noconfirm --needed jdk17-openjdk rustup
 
 archlinux-java set java-17-openjdk
 
