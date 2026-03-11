22fbec7097edc5f83851d49589240ab295c8c995
UnEmotioneD <blackeagle10@icloud.com>
2026-03-11 19:18:06 +0900

    feat: grub-config.sh

--- bin/grub-config.sh
+++ bin/grub-config.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if ! grep -q '^GRUB_DEFAULT=saved' /etc/default/grub; then
-  sudo sed -i 's/^GRUB_DEFAULT=.*/GRUB_DEFAULT=saved/' /etc/default/grub
-fi
+GRUB="/etc/default/grub"
 
-# TODO: need to know the defaults
+GRUB_OPTIONS=(
+  "GRUB_DEFAULT=saved"
+  "GRUB_GFXMODE=640x480"
+  "GRUB_DISABLE_LINUX_UUID=true"
+  "GRUB_DISABLE_RECOVERY=true"
+  "GRUB_SAVEDEFAULT=true"
+  "GRUB_DISABLE_SUBMENU=y"
+  "GRUB_DISABLE_OS_PROBER=false"
+)
 
-# GRUB_GFXMODE=640x480
-# GRUB_DISABLE_LINUX_UUID=true
-# GRUB_DISABLE_RECOVERY=true
-# GRUB_SAVEDEFAULT=true
-# GRUB_DISABLE_SUBMENU=y
-# GRUB_DISABLE_OS_PROBER=false
+for opt in "${GRUB_OPTIONS[@]}"; do
+  key="${opt%%=*}"
+  if grep -qE "^#?${key}=" "$GRUB"; then
+    sudo sed -Ei "s/^#?${key}=.*/${opt}/" "$GRUB"
+  else
+    echo "$opt" | sudo tee -a "$GRUB" >/dev/null
+  fi
+done
 
 # Update GRUB config after it's done
 sudo grub-mkconfig -o /boot/grub/grub.cfg
