2804dd469ee476e68f80e03c6318f2a9fe3bb627
UnEmotioneD <blackeagle10@icloud.com>
2026-03-12 19:14:37 +0900

    feat: check nm-applet before kill

--- bin/disable-nmapplet.sh
+++ bin/disable-nmapplet.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-sudo sh -c 'echo "Hidden=true" >> /etc/xdg/autostart/nm-applet.desktop'
-
-killall -9 nm-applet
-
-sudo pacman -Rns nm-applet
+if pacman -Qi networkmanager-applet >/dev/null; then
+    sudo sh -c 'echo "Hidden=true" >> /etc/xdg/autostart/nm-applet.desktop'
+    killall -9 nm-applet
+    sudo pacman -Rns --noconfirm networkmanager-applet
+fi
