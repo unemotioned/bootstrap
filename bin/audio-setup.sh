bf9bde8da63532837d456401865804f27ff37fb5
UnEmotioneD <blackeagle10@icloud.com>
2026-03-12 19:14:10 +0900

    fix: install pipewire-pule separately

--- bin/audio-setup.sh
+++ bin/audio-setup.sh
@@ -7,6 +7,7 @@ sudo pacman -S --noconfirm --needed\
   bolt\
   pavucontrol\
   pipewire-audio\
+  pipewire-pulse\
   sof-firmware
 
 systemctl --user enable --now pipewire pipewire-pulse wireplumber
