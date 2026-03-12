1e3ff1afe90256c1f00a79d3c86ea288a00f2b3d
UnEmotioneD <blackeagle10@icloud.com>
2026-03-13 02:14:02 +0900

    feat: reboot - print hehe cat

--- src/prompt-reboot.sh
+++ src/prompt-reboot.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if ! pacman -Qi chafa &>/dev/null; then
+  sudo pacman -S --noconfirm chafa
+fi
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+clear
+
+echo ''
+echo '====================================================='
+echo 'Installation and configuration completed successfully'
+echo '====================================================='
+echo ''
+chafa "$SCRIPT_DIR/../archive/hehe_kitty.png"
+
 read -rp 'Reboot now? [y/N] ' answer
 
 if [[ "${answer,,}" == 'y' ]]; then
