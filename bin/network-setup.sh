b32b0a1bc6ff048fbe6ded47d6ebf247ba3941b2
UnEmotioneD <blackeagle10@icloud.com>
2026-03-13 02:13:39 +0900

    feat: network - connect to wifi with iwctl

--- bin/network-setup.sh
+++ bin/network-setup.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 sudo pacman -S --noconfirm --needed\
+  iwd\
   impala\
   dhcpcd
 
@@ -13,3 +14,13 @@ fi
 sudo systemctl disable --now NetworkManager
 sudo systemctl enable --now iwd
 sudo systemctl enable --now dhcpcd
+
+if ! ping -c 1 -W 1 8.8.8.8 &>/dev/null; then
+  read -r -p "WiFi SSID: " ssid
+  read -r -sp "WiFi Password: " password
+  echo
+
+  device=$(iwctl device list | awk '/wlan/ {print $2; exit}')
+  iwctl --passphrase="$password" station "$device" connect "$ssid"
+  dhcpcd "$device"
+fi
