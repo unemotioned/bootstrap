d1bde0813f7d2843e020f87bb96ef54d1901851e
UnEmotioneD <blackeagle10@icloud.com>
2026-03-13 22:40:27 +0900

    feat: noconfirm to remove orphaned

--- bin/remove_orphaned.sh
+++ bin/remove_orphaned.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 mapfile -t orphans < <(yay -Qdtq || true)
 if [ ${#orphans[@]} -gt 0 ]; then
-  yay -Rns "${orphans[@]}"
+  yay -Rns --noconfirm "${orphans[@]}"
 else
   echo 'No orphaned packages found.'
 fi
