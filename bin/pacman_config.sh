1c1268684389551f242795bf7050bd016eb5e6a0
unemotioned <blackeagle10@icloud.com>
2026-03-14 02:52:33 +0900

    chore: cleanup pacman scripts

--- bin/pacman_config.sh
+++ bin/pacman_config.sh
@@ -1,32 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# check if Color is already set
-if grep -q '^Color' /etc/pacman.conf; then
-  echo 'Color: already set, skipping'
-else
-  sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
-  echo 'Color: set'
+FILE='/etc/pacman.conf'
+
+# uncomment Color
+if ! grep -q '^Color' "$FILE"; then
+  sudo sed -i 's/^#Color/Color/' "$FILE"
 fi
 
-# check if ILoveCandy is already set
-if grep -q '^ILoveCandy' /etc/pacman.conf; then
-  echo 'ILoveCandy: already set, skipping'
-else
-  sudo sed -i '/^Color/a ILoveCandy' /etc/pacman.conf
-  echo 'ILoveCandy: set'
-fi
-
-# print result of Color
-if grep -q '^Color' /etc/pacman.conf; then
-  echo 'Color: Success'
-else
-  echo 'Color: Failed'
-fi
-
-# print result of ILoveCandy
-if grep -q '^ILoveCandy' /etc/pacman.conf; then
-  echo 'ILoveCandy: Success'
-else
-  echo 'ILoveCandy: Failed'
+# add ILoveCandy after Color
+if ! grep -q '^ILoveCandy' "$FILE"; then
+  sudo sed -i '/^Color/a ILoveCandy' "$FILE"
 fi
