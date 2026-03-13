3bc1afcca82b9b930d6fffa8aab7068c46af4fd2
UnEmotioneD <blackeagle10@icloud.com>
2026-03-13 22:41:06 +0900

    feat(zoxide): check dir before adding

--- bin/zoxide_add.sh
+++ bin/zoxide_add.sh
@@ -14,12 +14,23 @@ dirs=(
   ".config/wofi"
   ".config/yazi"
   ".local/scripts"
+  "personal"
   "personal/bootstrap"
   "personal/tistory"
   "Pictures/Wallpapers"
   "Pictures/Screenshots"
+  "repo"
+  "repo/tmux-tokyo-night"
+  "repo/tokyonight.nvim"
 )
 
 for dir in "${dirs[@]}"; do
-  zoxide add "$HOME"/"$dir"
+  path="$HOME/$dir"
+
+  if [ -d "$path" ]; then
+    zoxide add "$path"
+    echo "Added '$path' to zoxide."
+  else
+    echo "'$path' does not exists."
+  fi
 done
