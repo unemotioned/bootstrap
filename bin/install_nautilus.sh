449eb9a53caaaf45af778537ecdf29f782edd888
unemotioned <blackeagle10@icloud.com>
2026-03-14 13:56:40 +0900

    add: catppuccin gtk theme in nautilus

--- bin/install_nautilus.sh
+++ bin/install_nautilus.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo pacman -S --noconfirm --noconfirm nautilus
+yay -S --noconfirm --needed\
+  nautilus\
+  catppuccin-gtk-theme-mocha
 
 if pacman -Qi dolphin &>/dev/null; then
   sudo pacman -Rns dolphin
 fi
 
-rm -rf ~/.local/share/dolphin ~/.local/state/dolphinstaterc
+rm -rf\
+  ~/.local/share/dolphin\
+  ~/.local/state/dolphinstaterc
