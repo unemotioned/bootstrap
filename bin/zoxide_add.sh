8f5fe09aa8e7027e68a030412e23d9df6642650d
unemotioned <blackeagle10@icloud.com>
2026-03-15 16:18:58 +0900

    feat: create missing dirs w/ xdg-user-dirs-update

--- bin/zoxide_add.sh
+++ bin/zoxide_add.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mkdir -p\
-  ~/Documents\
-  ~/Downloads\
-  ~/playground
+# standardize directories
+xdg-user-dirs-update
+
+mkdir -p ~/playground
 
 dirs=(
   dotfiles
