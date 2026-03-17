c5f498d732400d84263571374480cea072283583
unemotioned <blackeagle10@icloud.com>
2026-03-17 22:42:49 +0900

    feat: keyd - copy conf files from asset/keyd/

--- bin/install_keyd.sh
+++ bin/install_keyd.sh
@@ -4,51 +4,24 @@ set -euo pipefail
 sudo pacman -S --noconfirm --needed keyd
 
 dir='/etc/keyd'
-def="$dir/default.conf"
-kbd="$dir/kbd.conf"
-ball="$dir/ball.conf"
+def="$dir/all.conf"
+kbd="$dir/builtin.conf"
+ball="$dir/trackball.conf"
+
+asset="$EXE_PATH/asset/keyd"
 
 sudo mkdir -p "$dir"
 
 if [ ! -f "$def" ]; then
-  sudo tee "$def" <<EOF
-[ids]
-
-*
-
-[main]
-
-rightalt = f16
-EOF
+  sudo cp "$asset/all.conf" "$dir"
 fi
 
 if [ ! -f "$kbd" ]; then
-  sudo tee "$kbd" <<EOF
-[ids]
-
-# AT Translated Set 2 keyboard
-0001:0001:09b4e68d
-
-[main]
-
-rightalt = f16
-capslock = overload(control, esc)
-esc = capslock
-EOF
+  sudo cp "$asset/builtin.conf" "$dir"
 fi
 
 if [ ! -f "$ball" ]; then
-  sudo tee "$ball" <<EOF
-[ids]
-
-# splitkb.com Aurora Sofle v2 rev1
-047d:2041:14bbedc4
-
-[main]
-
-rightmouse = mouse1
-mouse1 = rightmouse
-EOF
+  sudo cp "$asset/trackball.conf" "$dir"
 fi
 
 if ! systemctl is-active --quiet keyd; then
