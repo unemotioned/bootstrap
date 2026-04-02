5067f39ecd938f124c362dde0f6c6947eb9c973c
unemotioned <blackeagle10@icloud.com>
2026-04-02 12:27:26 +0900

    add: go to language

--- src/install_lang.sh
+++ src/install_lang.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo pacman -S --noconfirm --needed jdk17-openjdk rustup
+sudo pacman -S --noconfirm --needed jdk17-openjdk rustup go
 
 sudo archlinux-java set java-17-openjdk
 
