fa9d64eedcc87f6bde42bf2588005d669e5d351c
unemotioned <blackeagle10@icloud.com>
2026-04-04 18:54:36 +0900

    Revert "feat: use NetworkManager + nmgui instead of iwd + impala"
    This reverts commit 6fe35e7beb0866149a386a3777032187acd80d3f.

--- src/network_setup.sh
+++ src/network_setup.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# GTK Wi-Fi connection manager
-if ! yay -Qi nmgui-bin &>/dev/null; then
-  yay -S --noconfirm nmgui-bin
+sudo pacman -S --noconfirm --needed impala iwd dhcpcd
+
+# disable auto start
+if [ -f /etc/xdg/autostart/nm-applet.desktop ]; then
+  sudo sh -c 'echo "Hidden=true" >> /etc/xdg/autostart/nm-applet.desktop'
+fi
+
+sudo systemctl disable NetworkManager
+sudo systemctl enable iwd dhcpcd
+
+if ! ping -c 1 -W 1 8.8.8.8 &>/dev/null; then
+  read -r -p "Wi-Fi SSID: " ssid
+  read -r -sp "Wi-Fi PW: " password
+  echo
+
+  device=$(iwctl device list | awk '/wlan/ {print $2; exit}')
+  iwctl --passphrase="$password" station "$device" connect "$ssid"
 fi
