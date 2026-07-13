9d77a2fb221e260c89e10d380657c0352006b735
unemotioned <134937260+unemotioned@users.noreply.github.com>
2026-07-13 12:03:29 +0900

    typo(vim.sh): noconform -> noconfirm

--- src/vim.sh
+++ src/vim.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 # install vim
-yay -S --noconform --needed vim
+yay -S --noconfirm --needed vim
 
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # install coc-sh dependencies
-yay -S --noconform --needed shellcheck shfmt
+yay -S --noconfirm --needed shellcheck shfmt
